2945c192bc9f0844496eb5ccda464185b938ae77
friggen <fredrf10@uia.no>
2021-11-05 10:35:43 +0100

    fixed long lat mix up

--- App.py
+++ App.py
@@ -17,56 +17,49 @@ from io import BytesIO
 import numpy as np
 import requests
 from PIL import Image
-
-
-
+    print('Please input desired latitude within Norway, "north-south direction" http://bboxfinder.com/#0.000000,0.000000,0.000000,0.000000')
+    # latitude ->   North-south ("vertical direction"),   latitude are the "horizontal lines" of the globe,
+    # -90 south pole, 0 equator, 90 north pole.
+    # 04.11Fredrik changed lat vs long implementation. and the adjusted the bounds
+    # changes are made in multiple lines to changes the lat vs long implementation, the lat was implemented as long. and visa verca
 def import_lat():
-    print('Please input desired latitude within Norway')
-    lat = float(input())
 
-    if lat > -16.1 and lat < 32.88:
-        lat = lat
+    lat = float(input())
+    if lat > 57.00 and lat < 71.00:
+            lat = lat
     else:
-        print('Latitude out of bounds. Value needs to be between -16.1 and 32.88')
+        print('Latitude out of bounds. Value needs to be between 57 and 71')
         import_lat()
 
     return lat
 
-
 def import_lon():
     print('Please input desired longitude within Norway')
     lon = float(input())
+    if lon > 2.0 and lon < 32.88:
 
-    if lon > 40.18 and lon < 84.17:
         lon = lon
     else:
-        print('Longitude out of bounds. Value needs to be between 40.18 and 84.17')
+        print('Longitude out of bounds. Value needs to be between 2 and 32.8')
         import_lon()
 
     return lon
-
-
 lat = import_lat()
 lon = import_lon()
-
-sq = 32                                    #This controls size of printout area, sq is side of square in kilometers
+sq = 2                    # value for printing "Gaustatoppen"          #This controls size of printout area, sq is side of square in kilometers
 corner_const = 90*sq/22000
 
-#lat = 16.8                                #Example input values for debugging purposes - Remove before final release
-#lon = 68.55                               #Example input values for debugging purposes - Remove before final release
+lat = 59.853952  # value for printing "Gaustatoppen"        #Example input values for debugging purposes - Remove before final release
+lon = 8.648471    # value for printing "Gaustatoppen"        #Example input values for debugging purposes - Remove before final release
 
-BBX = [lon - corner_const * 2, lon + corner_const * 2]
-BBY = [lat - corner_const, lat + corner_const]
+BBY = [lon - corner_const * 2, lon + corner_const * 2]
+BBX = [lat - corner_const, lat + corner_const]
 
+#BBOX=min_lat,min_long,max_lat,max_long:
 #transformer = Transformer.from_crs('WGS84', 'EPSG:25833')
 #BBOX_X, BBOX_Y = transformer.transform(BBX, BBY)
 
-# This is directly the API call used by Geonorge here:
-# https://kartkatalog.geonorge.no/kart?lat=6882011.719407242&lon=68429.52888256384&zoom=8.589318931685455
-# .. when adding the "Digital overflatemodell WMS layer".
-# For use in an application, please see requests user manual: https://docs.python-requests.org/en/latest/
-# See the following examples on how to handle http request parameters / payload and so on:
-# https://docs.python-requests.org/en/latest/user/quickstart/#passing-parameters-in-urls
+
 request_url = 'https://wms.geonorge.no/skwms1/wms.hoyde-dom?' \
            'SERVICE=WMS&' \
            'VERSION=1.3.0&' \
@@ -91,5 +84,3 @@ img = Image.fromarray(np.uint8(np_img))
 img.show()
 
 # Could convert to STL here.
-
-
